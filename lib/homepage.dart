c6b3a23b7d61a644bf4b9e9197a224b635d3137c
yukselballica <yukselballica2@gmail.com>
2024-09-03 16:20:59 +0300

    Flutter ToDoApp

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,36 +9,94 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  final _controller = TextEditingController();
   List toDoList = [
-    ["flutter öğren", false],
-    ["staja git", true],
+    ["melisayı kursa götür", false],
+    ["staja git, flutter çalış konuları bitir", true],
   ];
 
-  void checBoxChanged(int index){
+  void checBoxChanged(int index) {
     setState(() {
-      toDoList[index][1]=!toDoList[index][1];
+      toDoList[index][1] = !toDoList[index][1];
+    });
+  }
+
+  void saveNewTask() {
+    if (_controller.text.trim().isEmpty) {
+      // Kullanıcıya boş değer kaydedilemez mesajı göster
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Boş görev kaydedilemez!'),
+          backgroundColor: Colors.redAccent.shade700,
+        ),
+      );
+    } else {
+      setState(() {
+        toDoList.add([_controller.text, false]);
+      });
+      _controller.clear(); // Metin kutusunu temizler
+    }
+  }
+
+  void deleteTask(int index) {
+    setState(() {
+      toDoList.removeAt(index);
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFeda28a),
+      resizeToAvoidBottomInset: true,
+      backgroundColor: Colors.deepOrange.shade200,
       appBar: AppBar(
         title: const Text('Yapılacaklar Listesi'),
         centerTitle: true, // title ortalama
-        backgroundColor: Colors.deepPurple,
+        backgroundColor: Colors.deepOrange.shade700,
         foregroundColor: Colors.white,
       ),
       body: ListView.builder(
-          itemCount: toDoList.length,
-          itemBuilder: (BuildContext context, index) {
-            return todolist(
-              taskname: toDoList[index][0],
-              taskcompleted: toDoList[index][1],
-              onChanged:(value)=>checBoxChanged(index),
-            );
-          }),
+        itemCount: toDoList.length,
+        itemBuilder: (BuildContext context, index) {
+          return todolist(
+            taskname: toDoList[index][0],
+            taskcompleted: toDoList[index][1],
+            onChanged: (value) => checBoxChanged(index),
+            deleteFunction: (value) => deleteTask(index),
+          );
+        },
+      ),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20),
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: TextField(
+                  controller: _controller,
+                  decoration: InputDecoration(
+                    hintText: "Yeni görev ekle",
+                    hintStyle: TextStyle(
+                      color: Colors.black87,
+                    ),
+                    filled: true,
+                    fillColor: Colors.purple.shade50,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            FloatingActionButton(
+              onPressed: saveNewTask,
+              child: Icon(Icons.add),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/todolist.dart
+++ lib/todolist.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class todolist extends StatelessWidget {
   const todolist({
     super.key,
     required this.taskname,
     required this.taskcompleted,
-    this.onChanged,
+    required this.onChanged,
+    required this.deleteFunction,
   });
 
   final String taskname;
   final bool taskcompleted;
   final Function(bool?)? onChanged;
+  final Function(BuildContext)? deleteFunction;
+
 
 
 
@@ -25,32 +29,51 @@ class todolist extends StatelessWidget {
         bottom: 0,
         top: 20,
       ),
-      child:  Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-          color: Color(0xFF4CE0B3),
-          borderRadius: BorderRadius.circular(15),
-        ),
-        child: Row(
+      child:  Slidable(
+        endActionPane: ActionPane(
+          motion: StretchMotion(),
           children: [
-            Checkbox(
-                value: taskcompleted,
-                onChanged: onChanged,
-              checkColor: Colors.white,
-              activeColor: Colors.red,
-              side: BorderSide(
-                color: Colors.black,
-              ),
-            ),
-            Text(
-              taskname,
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                decoration: TextDecoration.lineThrough,
-              ),
+            SlidableAction( // Burada doğrudan ekledik
+              onPressed: deleteFunction,
+              icon: Icons.delete,
+              borderRadius: BorderRadius.circular(15),
+              backgroundColor: Colors.red,
             ),
           ],
+        ),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Color(0xFF4CE0B3),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          child: Row(
+            children: [
+              Checkbox(
+                  value: taskcompleted,
+                  onChanged: onChanged,
+                checkColor: Colors.white,
+                activeColor: Colors.red,
+                side: BorderSide(
+                  color: Colors.black,
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  taskname,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18,
+                    decoration: taskcompleted? TextDecoration.lineThrough:TextDecoration.none,
+                    decorationColor: Colors.black,
+                    decorationThickness: 2,
+                  ),
+                  maxLines: 3, // Metnin max 3 satıra kadar genişlemesi için
+                  overflow: TextOverflow.ellipsis, // Metin taşmasını engelliyor
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
